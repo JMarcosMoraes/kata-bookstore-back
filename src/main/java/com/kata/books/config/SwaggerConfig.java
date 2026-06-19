5a9dd6547074222401549b3222e2f224ad4856e2
José Marcos Vilas Boas Moraes <josemarcosmoraes@gmail.com>
2026-06-19 18:41:16 -0300

    V0-Configurando Swagger

--- src/main/java/com/kata/books/config/SwaggerConfig.java
+++ src/main/java/com/kata/books/config/SwaggerConfig.java
@@ -4,32 +4,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 
-import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.any()) // todos os endpoints
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .securitySchemes(List.of(apiKey()))
+                .securityContexts(List.of(securityContext()));
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Bookstore API",
-                "Documentação automática de todos os endpoints",
-                "1.0",
-                "Terms of service",
-                new Contact("José Marcos", "https://kata-bookstore-front.vercel.app/", "email@dominio.com"),
-                "License of API", "API license URL", Collections.emptyList());
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope =
+                new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
     }
 }
